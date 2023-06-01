be7151838117effa2e779c69afacc3e855da71aa
류종학 <hy5849@gmail.com>
2023-06-01 15:29:08 +0900

    route.py 코드 리팩터링 및 주석 수정

--- server/route.py
+++ server/route.py
@@ -1,83 +1,63 @@
-# -*- coding: utf-8 -*-
-import requests
-from flask import Flask, Response, request, send_file, jsonify, render_template
-from flask_restx import Api, Resource, fields, Namespace  # Api 구현을 위한 Api 객체 import
-
+import random
+from flask import Flask, request
+from flask_restx import Api, Resource
 from database.database import S3
 from database.sql import Sql
 from services.foot import Foot
 import services.ocr as ocr
 
-import random
-
-app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
+# Swagger(flask_restx) Configuration
+app = Flask(__name__)
 api = Api(app, version='1.0', title='Carenco Flask Server',
-          description='Carenco Flask Server 입니다. 각 Api들을 테스트할 수 있습니다.')  # Flask 객체에 Api 객체 등록
+          description='Carenco Flask Server 입니다. 각 Api들을 테스트할 수 있습니다.')
 
+class BaseService(Resource):
+    def __init__(self):
+        self.sql = Sql()
+        self.foot = Foot()
+        self.s3 = S3()
 
 @api.route('/image')
-class Image(Resource):
-    def post(self):
-        '''ID 값을 이용하여 이미지를 가공하여 저장하는 Api'''
-        try:
-            params = request.get_json(force=True)
-            id = params['id']
-            print("id값 {}".format(id))
+class Image(BaseService):
+    def put(self) -> str:
+        ''' ID 값을 받아 사용자를 특정하고 이미지와 몸무게를 저장하는 Api '''
+        params = request.get_json(force=True)
+        if 'id' not in params:
+            return {"error": "invalid request parameters: 'id' not found"}, 400
 
-            foot = Foot()
-            _, weight_values = foot.generate_image(params, './')
-            standard_num = random.randint(1, 8) ##임시 1-8 사이의 번호 생성
-
-            s3 = S3()
-            image_url = s3.ImageUploadToS3(id)
-
-            sql = Sql()
-            weight = weight_values  # random.randrange(40, 100)
-            sql.save(id,image_url,weight,standard_num)
-            description = sql.find_description(standard_num)
-
-            print(description[0])
-
-            #return image_url
-            return jsonify({'url' : image_url, 'weight' : weight, 'description' : description})
-
-        except KeyError:
-            return {"error": "invalid request parameters"}, 400
+        id = params['id']
+        _, weight_values = self.foot.generate_image(params, './')
+        image_url = self.s3.ImageUploadToS3(id)
+        self.sql.save(id, image_url, weight_values)
+        return image_url
 
 
 @api.route("/health")
 class Health(Resource):
     def get(self):
-        '''Test Api'''
+        ''' Health Test Api '''
         return ""
 
 
 @api.route("/check")
 class Check(Resource):
     def get(self):
-        '''Check Test Api'''
+        ''' Check Test Api '''
         return "check"
 
 
 @api.route('/ocr')
-class Ocr(Resource):
-    def put(self):
-        '''Inbody 이미지를 Request시 구글 Vision OCR Api를 이용하여 Response 데이터를 가공, DB에 데이터 저장하는 Api'''
-        try:
-            foot = Foot()
-
-            # request로 넘어오는 이미지
-            file = request.files['file']
-            data = ocr.googleVision.google_ocr(file)
-
-            sql = Sql()
-            sql.create_health_info(data)
-            return "test"
-        except KeyError:
+class Ocr(BaseService):
+    def put(self) -> str:
+        ''' 이미지 FILE을 받아 구글 Vision Api를 통해 OCR 진행하고 결과값을 받아 저장하는 Api '''
+        if 'file' not in request.files:
             return {"error": "image file not found in request"}, 400
 
+        file = request.files['file']
+        data = ocr.googleVision.google_ocr(file)
+        self.sql.create_health_info(data)
+        return "test"
 
 
-# 기본 값 9000
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9000)
